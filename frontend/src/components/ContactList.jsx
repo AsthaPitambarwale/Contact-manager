24de5839e9b8842b33acb30e3680756660682ae2
AsthaPitambarwale666 <167300135+AsthaPitambarwale@users.noreply.github.com>
2026-01-03 16:38:42 +0530

    Update ContactList.jsx

--- frontend/src/components/ContactList.jsx
+++ frontend/src/components/ContactList.jsx
@@ -5,7 +5,7 @@ export default function ContactList({ contacts, refresh }) {
   const deleteContact = async (id) => {
   try {
     console.log("Deleting ID:", id);
-    await axios.delete(`http://localhost:5000/api/contacts/${id}`);
+    await axios.delete(`${process.env.REACT_APP_API_URL}/api/contacts/${id}`);
     refresh(); 
   } catch (error) {
     console.error("Delete failed:", error);
