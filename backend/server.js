8b813fbeddaca4b42a2abb01fb8b73f9e21ed272
AsthaPitambarwale666 <167300135+AsthaPitambarwale@users.noreply.github.com>
2026-01-03 18:13:25 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,11 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
 app.use(cors({
   origin: process.env.FRONTEND_URL || "*"
 }));
+
 app.use(express.json());
 
 app.use("/api/contacts", require("./routes/contactRoutes"));
