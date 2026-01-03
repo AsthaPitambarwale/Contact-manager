d68688e50f8e550e5283a729eade7bf75725739a
AsthaPitambarwale666 <167300135+AsthaPitambarwale@users.noreply.github.com>
2026-01-03 16:55:48 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const app = express();
 const connectDB = require("./config/db");
 
 dotenv.config();
