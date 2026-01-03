f29919da9d8828ad568c5d81a08e4cc5ccda7cad
AsthaPitambarwale666 <167300135+AsthaPitambarwale@users.noreply.github.com>
2026-01-03 16:50:36 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const app = express();
 const connectDB = require("./config/db");
 
 dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "https://contact-manager-udsf.vercel.app"
+}));
 app.use(express.json());
 
 app.use("/api/contacts", require("./routes/contactRoutes"));
